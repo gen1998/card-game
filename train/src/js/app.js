22d0b1b434fff92129827bef1ee2c3623fcaea96
root <root@DESKTOP-55VQPS9.localdomain>
2020-10-04 23:00:39 +0900

    設定ページとゲームページへの遷移対応

--- train/src/js/app.js
+++ train/src/js/app.js
@@ -1,80 +1,114 @@
+//キャンバスの描画・全体設定（幅・高さ）
 var c = document.createElement("canvas");
 var ctx = c.getContext("2d");
- c.width = window.innerWidth;
- c.height = window.innerHeight;
- //c.width = 600;
-//c.height = 400;
+
+c.width = window.innerWidth;
+c.height = window.innerHeight;
 
 document.body.appendChild(c);
 
-/*
-let shikaku = new function(){
-  this.draw = function(){
-    c.addEventListener('click', test);
-    console.log("hey!");
-  }
-}
-*/
-
+//初期画面ボタン編
 function start_buttons(){
-  this.x = c.width / 2;
-  this.y = c.height / 2;
-  
-  let start_x = 300;
-  let start_y = 100;
-  
-  let setting_x = 100;
-  let setting_y = 100;
 
-  
+  //ボタン全体設定
   ctx.fillStyle = 'white';
-  ctx.fillRect(c.width/2-start_x/2, c.width/2-start_y/2, start_x, start_y);
-  ctx.fillRect(c.width-setting_x-50, c.width-setting_y-50, setting_x, setting_y);
-  //ctx.rect(this.x-this.img.width/2, 40, 100, 100);
-  ctx.stroke();
+
+  //スタートボタン座標設定
+  let start_w = 300;
+  let start_h = 100;
+  let start_x = c.width/2-start_w/2;
+  let start_y = c.width/2-start_h/2;
   
+  //スタートボタン
+  ctx.fillRect(start_x, start_y, start_w, start_h);
+  
+  //設定ボタン座標設定
+  let setting_w = 100;
+  let setting_h = 100;
+  let setting_x = c.width-setting_w*3/2;
+  let setting_y = c.width-setting_h*3/2;
+
+  //設定ボタン
+  ctx.fillRect(setting_x, setting_y, setting_w, setting_h);
+  
+  //文字の全体設定
   ctx.fillStyle = 'rgba(0,0,0)';
   ctx.textAlign = "center";
-  ctx.textBaseline = "middle";
-
+  ctx.textBaseline = "middle"; //座標の開始位置を中央に設定！
+  
+  //スタートボタンの文字
+  let start_letter_x=c.width/2;
+  let start_letter_y=c.width/2;
   ctx.font = '40pt メイリオ';
-  ctx.fillText('スタート', c.width/2, c.width/2);
-
+  ctx.fillText('スタート', start_letter_x, start_letter_y);
+　
+  //設定の文字
+  let setting_letter_x=c.width-setting_h;
+  let setting_letter_y=c.width-setting_w;
   ctx.font = '30pt メイリオ';
-  ctx.fillText('設定',c.width-setting_x, c.width-setting_y);
+  ctx.fillText('設定',setting_letter_x, setting_letter_y);
 
+  //商標の文字
+  let trademark_setting_x=150;
+  let trademark_setting_y=c.width-80;
   ctx.fillStyle = 'white';
   ctx.font = '15pt メイリオ';
-  ctx.fillText('©2020 Musashi 91期 inc.', 150, c.width-80); 
-
-  console.log("shikaku detayo");
+  ctx.fillText('©2020 Musashi 91期 inc.', trademark_setting_x, trademark_setting_y); 
 }
 
-
-c.addEventListener('click',function(e){
-  if(!stop_title_flag){
-    ctx.clearRect(0,0,c.width,c.height);
-  }
-  stop_title_flag=true;
-},false);
-
-
-let stop_title_flag = false;
-
-
+//初期画面ロゴ編
 let logo = new function() {
-  this.x = c.width / 2;
-  this.y = c.height / 5;
-  this.rot = 0;
+  let x = c.width / 2;
+  let y = c.height / 5;
+  let rot = 0;
 
-  this.draw = function() {
+  this.draw = function(){ //このthisはこのままでーす！letじゃ動かないです。なんで？
     this.img = new Image();
     this.img.src = "images/logo.png";
-    ctx.drawImage(this.img, this.x-this.img.width/2 ,this.y-this.img.height/2);
-    console.log("logo display!");
+    let logo_x = x-this.img.width/2;
+    let logo_y = y-this.img.height/2;
+    ctx.drawImage(this.img, logo_x ,logo_y);
   }
 }
 
+//フラグ集（デフォルトはfalse）
+let stop_title_flag = false;
+let start_setting_flag=false;
+let start_game_flag=false;
+
+//変数再定義（改善したいです）
+let start_w = 300;
+let start_h = 100;
+let start_x = c.width/2-start_w/2;
+let start_y = c.width/2-start_h/2;
+let setting_w = 100;
+let setting_h = 100;
+let setting_x = c.width-setting_w*3/2;
+let setting_y = c.width-setting_h*3/2;
+
+//クリックしたらフラグ起動
+c.addEventListener('click',function(e){
+  if(!stop_title_flag){
+    var rect = e.target.getBoundingClientRect();
+    console.log(e.clientX)
+    
+    if(start_x <= e.clientX && e.clientX <= start_x+start_w && start_y <= e.clientY && e.clientY <= start_y+start_h){
+      ctx.clearRect(0,0,c.width,c.height); 
+      stop_title_flag=true; 
+      start_game_flag=true;
+      
+    }
+    
+    if(setting_x <= e.clientX && e.clientX <= setting_x+setting_w && setting_y <= e.clientY && e.clientY <= setting_y+setting_h){
+      ctx.clearRect(0,0,c.width,c.height);
+      stop_title_flag=true; 
+      start_setting_flag=true;
+    }    
+  }
+});
+
+
+//表示
 function loop(){
   if(!stop_title_flag){
     ctx.fillStyle = "#005731";
@@ -83,6 +117,16 @@ function loop(){
     start_buttons();
     logo.draw();
     requestAnimationFrame(loop);
+  }
+  if(start_setting_flag){
+    ctx.fillStyle = "#005731";
+    ctx.globalAlpha = 1.0;
+    ctx.fillRect(0, 0, c.width, c.height);
+  }
+  if(start_game_flag){
+    ctx.fillStyle = "#005731";
+    ctx.globalAlpha = 1.0;
+    ctx.fillRect(0, 0, c.width, c.height);
   }
 }
 
