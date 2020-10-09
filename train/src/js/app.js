a19b791f8bd3ecaeac082a5803455bad1e98955f
yuuto06 <gitgithjkoI66>
2020-10-09 22:07:15 +0900

    update README

--- train/src/js/app.js
+++ train/src/js/app.js
@@ -90,7 +90,7 @@ let setting_y = c.width-setting_h*3/2;
 c.addEventListener('click',function(e){
   if(!stop_title_flag){
     var rect = e.target.getBoundingClientRect();
-    console.log(e.clientX)
+    //console.log(e.clientX)
     
     if(start_x <= e.clientX && e.clientX <= start_x+start_w && start_y <= e.clientY && e.clientY <= start_y+start_h){
       ctx.clearRect(0,0,c.width,c.height); 
@@ -107,6 +107,116 @@ c.addEventListener('click',function(e){
   }
 });
 
+//プレイ画面カード表示
+let card = new function() {
+  let x = c.width / 2;
+  let y = c.height / 2;
+  let rot = 0;
+  
+  this.draw = function(){ //このthisはこのままでーす！letじゃ動かないです。なんで？
+    this.img = new Image();
+    this.img.src = "images/heart3.jpg";
+    let card_x = x-this.img.width/2;
+    let card_y = y-this.img.height/2;
+    ctx.drawImage(this.img, card_x ,card_y);
+  }
+}
+
+//プレイ画面プレーヤー表示1
+let player1 = new function() {
+  let x = c.width / 2;
+  let y = c.height / 5;
+  let rot = 0;
+  
+  this.draw = function(){ //このthisはこのままでーす！letじゃ動かないです。なんで？
+    this.img = new Image();
+    this.img.src = "images/player/player1.png";
+    let player_width = this.img.width/3;
+    let player_height =this.img.height/3;
+    let player_x = x-player_width/2;
+    let player_y = 0;
+    ctx.drawImage(this.img, player_x ,player_y,player_width,player_height);
+  }
+}
+
+//プレイ画面プレーヤー表示2
+let player2 = new function() {
+  let x = c.width /2;
+  let y = c.height / 2;
+  let rot = 0;
+  
+  this.draw = function(){ //このthisはこのままでーす！letじゃ動かないです。なんで？
+    this.img = new Image();
+    this.img.src = "images/player/player2.png";
+    let player_width = this.img.width/3;
+    let player_height =this.img.height/3;
+    let player_x = 0;
+    let player_y = y-player_height/2;
+    ctx.drawImage(this.img, player_x ,player_y,player_width,player_height);
+  }
+}
+
+//プレイ画面プレーヤー表示3
+let player3 = new function() {
+  let x = c.width /2;
+  let y = c.height / 2;
+  let rot = 0;
+  
+  this.draw = function(){ //このthisはこのままでーす！letじゃ動かないです。なんで？
+    this.img = new Image();
+    this.img.src = "images/player/player3.png";
+    let player_width = this.img.width/3;
+    let player_height =this.img.height/3;
+    let player_x = c.width-player_width;
+    let player_y = y-player_height/2;
+    ctx.drawImage(this.img, player_x ,player_y,player_width,player_height);
+  }
+}
+
+//プレイ画面ボタン編
+function play_buttons(){
+
+  //ボタン全体設定
+  ctx.fillStyle = 'white';
+
+  //決定ボタン座標設定
+  let decide_w =c.width/3;
+  let decide_h = c.height/10;
+  let decide_x = c.width/3-decide_w/2;
+  let decide_y = c.width/2-decide_h/2;
+  
+  //決定ボタン
+  ctx.fillRect(decide_x, decide_y, decide_w, decide_h);
+
+  //パスボタン座標設定
+  let pass_w =c.width/3;
+  let pass_h = c.height/10;
+  let pass_x = c.width*2/3-pass_w/2;
+  let pass_y = c.width/2-pass_h/2;
+    
+  //パスボタン
+  ctx.fillRect(pass_x, pass_y, pass_w, pass_h);
+
+  
+  //文字の全体設定
+  ctx.fillStyle = 'rgba(0,0,0)';
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle"; //座標の開始位置を中央に設定！
+  
+  //決定ボタンの文字
+  let decide_letter_x=c.width/3;
+  let decide_letter_y=c.width/2;
+  ctx.font = '40pt メイリオ';
+  ctx.fillText('カードを出す', decide_letter_x, decide_letter_y);
+
+   //パスボタンの文字
+   let pass_letter_x=c.width*2/3;
+   let pass_letter_y=c.width/2;
+   ctx.font = '40pt メイリオ';
+   ctx.fillText('パス', pass_letter_x, pass_letter_y);
+}
+
+
 
 //表示
 function loop(){
@@ -122,13 +232,22 @@ function loop(){
     ctx.fillStyle = "#005731";
     ctx.globalAlpha = 1.0;
     ctx.fillRect(0, 0, c.width, c.height);
+    requestAnimationFrame(loop);
   }
   if(start_game_flag){
     ctx.fillStyle = "#005731";
     ctx.globalAlpha = 1.0;
     ctx.fillRect(0, 0, c.width, c.height);
+    card.draw();
+    player1.draw();
+    player2.draw();
+    player3.draw();
+    play_buttons();
+    requestAnimationFrame(loop);
   }
 }
+
+
 
 loop();
 
